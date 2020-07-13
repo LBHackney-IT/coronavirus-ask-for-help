1565570374f9bd56c1fb0d0b2e232113f130a088
Jayson Hunter <github@jaysonhunter.com>
2020-07-13 16:49:33 +0100

    Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+'use strict'
+
+const paths = require('./config/paths.json')
+const gulp = require('gulp')
+const taskListing = require('gulp-task-listing')
+const taskArguments = require('./tasks/gulp/task-arguments')
+
+// Gulp sub-tasks
+require('./tasks/gulp/clean.js')
+require('./tasks/gulp/lint.js')
+require('./tasks/gulp/compile-assets.js')
+require('./tasks/gulp/nodemon.js')
+require('./tasks/gulp/watch.js')
+// new tasks
+require('./tasks/gulp/copy-to-destination.js')
+require('./tasks/gulp/asset-version.js')
+require('./tasks/gulp/sassdoc.js')
+
+// Umbrella scripts tasks for preview ---
+// Runs js lint and compilation
+// --------------------------------------
+gulp.task('scripts', gulp.series(
+  'js:compile'
+))
+
+// Umbrella styles tasks for preview ----
+// Runs js lint and compilation
+// --------------------------------------
+gulp.task('styles', gulp.series(
+  'scss:lint',
+  'scss:compile'
+))
+
+// Copy assets task ----------------------
+// Copies assets to taskArguments.destination (public)
+// --------------------------------------
+gulp.task('copy:img', () => {
+  return gulp.src(paths.src + 'assets/img/**/*')
+    .pipe(gulp.dest(taskArguments.destination + '/img/'))
+})
+
+// All test combined --------------------
+// Runs js, scss and accessibility tests
+// --------------------------------------
+gulp.task('test', gulp.series(
+  'scss:lint',
+  'scss:compile'
+))
+
+// Copy assets task for local & heroku --
+// Copies files to
+// taskArguments.destination (public)
+// --------------------------------------
+gulp.task('copy-assets', gulp.series(
+  'styles',
+  'scripts'
+))
+
+// Serve task ---------------------------
+// Restarts node app when there is changed
+// affecting js, css or njk files
+// --------------------------------------
+gulp.task('serve', gulp.parallel(
+  'watch',
+  'nodemon'
+))
+
+// Dev task -----------------------------
+// Runs a sequence of task on start
+// --------------------------------------
+gulp.task('dev', gulp.series(
+  'clean',
+  'copy-assets',
+  'copy:img',
+  'sassdoc',
+  'serve'
+))
+
+// Build package task -----------------
+// Prepare package folder for publishing
+// -------------------------------------
+gulp.task('build:package', gulp.series(
+  'clean',
+  'copy-files',
+  'js:compile'
+))
+gulp.task('build:dist', gulp.series(
+  'clean',
+  'copy-assets',
+  'copy:img',
+  'update-assets-version'
+))
+
+// Default task -------------------------
+// Lists out available tasks.
+// --------------------------------------
+gulp.task('default', taskListing)
