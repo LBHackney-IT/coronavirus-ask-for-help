6a7f7b598c4f07b0d7e5d43d6f2c0ec8cb96d96c
Riccardo Noviello <riccardo.noviello@gmail.com>
2020-08-06 12:57:18 +0100

    amend host ip for tests

--- test/app.spec..js
+++ test/app.spec..js
@@ -4,7 +4,7 @@ var request = require('request');
 describe("Application Tests", function() {
 
     it('Renders the index page', function(done) {
-        request('http://localhost:9000' , function(error, response, body) {
+        request('http://0.0.0.0:9000' , function(error, response, body) {
             expect(response.statusCode).to.equal(200)
             done();
         });
